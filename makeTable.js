797528b0f58e8502d2786fb6e0415af96ee768d0
Mathieu Loyer <loyer.mathieu@courrier.uqam.ca>
2016-04-28 17:55:36 -0400

    Update makeTable.js

--- makeTable.js
+++ makeTable.js
@@ -1,55 +1,12 @@
 $(document).ready(function() {
-	makeHTMLTable();
-	$('table').eq(0).DataTable();
+	Stades = JSON.parse(localStorage.getItem("stades")).data;
+	$("#dataTable").DataTable({
+			"data": Stades,
+    "columns": [
+        { "data": "nom" },
+        { "data": "dateService"}   
+    ]
+	});
 });
 
-function makeHTMLTable(){
-	
-	txt = localStorage.getItem("stades");
-	obj = JSON.parse(txt);
-	HTML = "";
-	
-	stadiumCount = countStadiums(obj);
-	
-	//alert("runngin");
-	
-	for(i = 0; i < stadiumCount; i++){
-		stadiumX = getNextStadium(obj, i); 
-		HTML += makeTR(stadiumX);
-	}
-	
-	//alert(HTML);
-	addHTMLToTable(HTML);
-	
-}
 
-function getNextStadium(obj, i){
-	//alert(obj.data[i].nom);
-	return obj.data[i];
-}
-
-function makeTR(stadiumX){
-	txt = "<TR>";
-	txt += "<TD>" + stadiumX.nom + "</TD>";
-	txt += "<TD>" + stadiumX.capacite + "</TD>";
-	txt += "</TR>";
-	return txt;
-}
-
-function addHTMLToTable(HTML){
-	$("thead").after(HTML);
-}
-
-function countStadiums(obj){
-	//alert(JSON.stringify(localStorage.getItem("stades")));
-	//alert(obj.data.length);
-	return obj.data.length;;
-}
-
-/*
- 	obj = {};
-	obj.nom = stadeXName;
-	obj.capacite = capaciteX;
-	obj.acces = accesX;
-	obj.environnement = environnementX;
- */
